d652b389b404a6c58b6c75589414a445eda58a48
Rylie Castell <rylie.castell@gmail.com>
2025-04-08 10:02:47 +0200

    add entry struct

---
+++ Password Manager/Entry.swift
@@ -0,0 +1,21 @@
+//
+//  Entry.swift
+//  Password Manager
+//
+//  Created by Rylie Castell on 08.04.25.
+//
+
+import Foundation
+
+// Aufgabe 1.2 Struct namens Eintrag modellieren
+
+struct Entry: CustomStringConvertible {
+    var id: Int
+    var website: String
+    var userName: String
+    var password: String
+    
+    var description: String {
+        "\(id). Website: \(website) - Username: \(userName) - Passwort: \(password)"
+    }
+}
